dc88b06191409f15c0b390659f12eab5b9e19cdf
João Silva <joaosilva7875@gmail.com>
2025-02-10 17:19:18 +0100

    added national pokedex

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,22 +1,23 @@
-import { data, Outlet } from "react-router";
+import { Outlet } from "react-router";
 import type { Route } from "./+types/home";
 import Header from "~/components/Header";
-import { Suspense, useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router";
 import PokeballLoadingIcon from "~/icons/pokeball.png";
-import PokemonContainer from "~/routes/PokemonContainer";
+import PokemonContainer from "~/components/PokemonContainer";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Emerald Pokedex" },
+    { name: "description", content: "Emerald Pokedex" },
   ];
 }
 
 export default function Home() {
   const [pokemonData, setPokemonData] = useState<Array<any>>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isPokedexNational, setIsPokedexNational] = useState(false);
 
   useEffect(() => {
     axios
@@ -34,6 +35,19 @@ export default function Home() {
         setIsLoading(false);
       });
   }, []);
+
+  const ShowNationalPokedex = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokedex/1")
+      .then((res) => {
+        const data = res.data.pokemon_entries;
+
+        setPokemonData(data);
+      })
+      .finally(() => {
+        setIsPokedexNational(true);
+      });
+  };
 
   const DeoxysContainer = () => {
     return (
@@ -76,7 +90,17 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
       <Header />
+      <button
+        onClick={ShowNationalPokedex}
+        className="flex items-center justify-center cursor-pointer"
+      >
+        <h1 className="text-xl">
+          <span>Click here to view the&nbsp;</span>
+          <span className="text-red-600">National</span> Pokédex instead.
+        </h1>
+      </button>
       <Outlet />
+
       {isLoading ? (
         <div className="flex flex-center items-center text-4xl mt-60">
           <div className="flex items-center">
@@ -84,7 +108,7 @@ export default function Home() {
             <img src={PokeballLoadingIcon} className="ml-2 h-8 w-8" />
           </div>
         </div>
-      ) : (
+      ) : isPokedexNational == false ? (
         <div className="flex flex-row justify-center flex-wrap p-10 pt-2">
           {pokemonData.slice(0, 201).map((data, index) => (
             <>
@@ -99,6 +123,21 @@ export default function Home() {
             </>
           ))}
           <DeoxysContainer />
+        </div>
+      ) : (
+        <div className="flex flex-row justify-center flex-wrap p-10 pt-2">
+          {pokemonData.map((data, index) => (
+            <>
+              <Link to={`/${data.pokemon_species.name}`}>
+                <PokemonContainer
+                  key={index}
+                  pokemonId={data.entry_number}
+                  pokemonName={data.pokemon_species.name}
+                  pokemonImg={`https://img.pokemondb.net/artwork/${data.pokemon_species.name}.jpg`}
+                />
+              </Link>
+            </>
+          ))}
         </div>
       )}
     </div>
